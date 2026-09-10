e511a543dcdd68586493e97449565c9c3bf8ab58
RAFFMONNN <rafaalrabbani08@gmail.com>
2026-09-10 17:27:05 +0700

    perluas initTableFilter

--- jobsheet-05/assets/js/app.js
+++ jobsheet-05/assets/js/app.js
@@ -34,7 +34,8 @@ function initTableFilter() {
         const keyword = input.value.toLowerCase();
         const rows = table.querySelectorAll("tbody tr");
         rows.forEach(function (row) {
-            const teks = row.querySelector("td")?.textContent.toLowerCase() || "";  
+            const teks = row.querySelector("td")?.textContent.toLowerCase() || ""; 
+            // perluas initTableFilter pada kode diatas
             row.style.display = teks.includes(keyword) ? "" : "none";
         });
         updateRowCounter();
